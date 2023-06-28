32ef677759a8664f959efb85de6df70d982cea7a
KseniaLune <ksenia00nuysha@gmail.com>
2023-06-28 16:52:19 +0300

    done

---
+++ src/main/java/org/example/HappyNumber.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(isHappy(n));
+    }
+
+    public static boolean isHappy(int n) {
+        if(n==1||n==7){
+            return true;
+        }else if (n<10){
+            return false;
+        }
+        String s = n + "";
+        char[] chars = s.toCharArray();
+
+        List<Integer> list = new ArrayList<>();
+        for (char c : chars) {
+            list.add(Character.getNumericValue(c));
+        }
+        int res = 0;
+        System.out.println(list);
+        for (int i : list) {
+            if (i !=0){
+                res = res + i * i;
+            }
+            System.out.println(res+" i= "+i);
+        }
+        if (res == 1){
+            System.out.println("res:"+res);
+            return true;
+
+        } else{
+            return isHappy(res);
+        }
+    }
+}
+
