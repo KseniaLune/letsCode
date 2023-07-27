b5ef9efa1994a00e4541cc95bd10fd7826857e6f
KseniaLune <ksenia00nuysha@gmail.com>
2023-07-27 18:38:40 +0300

    resolved

---
+++ src/main/java/org/example/quest/medium/RotateArray.java
@@ -0,0 +1,35 @@
+package org.example.quest.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2};
+        rotate(arr, 3);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void rotate(int[] nums, int k) {
+        if (k == 0) return;
+        if (nums.length < 2) return;
+        if (k == nums.length) return;
+        while (k> nums.length){
+            k-=nums.length;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int n = nums.length - k;
+        while (n < nums.length) {
+            list.add(nums[n]);
+            n++;
+        }
+        for (int j = 0; j < nums.length - k; j++) {
+            list.add(nums[j]);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            nums[j] = list.get(j);
+        }
+    }
+}
