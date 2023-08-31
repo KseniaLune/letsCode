8eadffabf51534ee5b8c27d08569321eb4587acc
KseniaLune <ksenia00nuysha@gmail.com>
2023-08-31 13:38:38 +0300

    new five sql problems

---
+++ src/main/java/org/example/quest/easy/FindCustomerReferee.sql
@@ -0,0 +1,27 @@
+/**Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+CREATE TABLE Customer(
+    id int primary key,
+    name varchar(255),
+    referee_id int
+);
+
+SELECT name FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL;
+
